623c071e13791876f5f4359ce95a4072ef435e8d
anpachecos <an.pachecos@duocuc.cl>
2024-11-20 12:29:37 -0300

    api rest lista

--- backend/backend_ionic/urls.py
+++ backend/backend_ionic/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import UsuarioListCreate, AsignaturaListCreate, HorarioAsignaturaListCreate, AsistenciaListCreate
+from .views import (
+    UsuarioListCreate, AsignaturaListCreate, HorarioAsignaturaListCreate,
+    AsistenciaListCreate, get_user_by_name
+)
 
 urlpatterns = [
     path('usuarios/', UsuarioListCreate.as_view(), name='usuario-list-create'),
+    path('usuarios/search/', get_user_by_name, name='get-user-by-name'),  # Nueva ruta para buscar por nombre
     path('asignaturas/', AsignaturaListCreate.as_view(), name='asignatura-list-create'),
     path('horarios/', HorarioAsignaturaListCreate.as_view(), name='horario-list-create'),
     path('asistencias/', AsistenciaListCreate.as_view(), name='asistencia-list-create'),
--- backend/backend_ionic/views.py
+++ backend/backend_ionic/views.py
@@ -1,6 +1,42 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Usuario, Asignatura, HorarioAsignatura, Asistencia
 from .serializers import UsuarioSerializer, AsignaturaSerializer, HorarioAsignaturaSerializer, AsistenciaSerializer
+
+
+
+@api_view(['GET'])
+def get_user_by_name(request):
+    """
+    Endpoint para buscar un usuario por su nombre.
+    """
+    nombre = request.query_params.get('search', None)  # Obtiene el parámetro 'search' de la URL
+    if nombre:
+        try:
+            usuario = Usuario.objects.get(nombre=nombre)
+            serializer = UsuarioSerializer(usuario)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Usuario.DoesNotExist:
+            return Response({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response({'error': 'Falta el parámetro de búsqueda'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class UsuarioListCreate(generics.ListCreateAPIView):
     queryset = Usuario.objects.all()
